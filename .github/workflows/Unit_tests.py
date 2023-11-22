3d2de280691ae0d0da263ca79f26b8907e2a893a
gleb421 <94608946+gleb421@users.noreply.github.com>
2023-11-22 12:22:05 +0300

    Update Unit_tests.py

--- .github/workflows/Unit_tests.py
+++ .github/workflows/Unit_tests.py
@@ -5,13 +5,13 @@ import unittest
 
 class CurrenciesCalculator(unittest.TestCase):
 
-    # def test_res(self):
-    #     res1 = 1 * 93.0351
-    #     res2 = 2 * 99.0111
-    #     res3 = 3 * 12.6911
-    #     self.assertEqual(res1, currenciesCalc(1, 'USD'))
-    #     self.assertEqual(res2, currenciesCalc(2, 'EUR'))
-    #     self.assertEqual(res3, currenciesCalc(3, 'CNY'))
+    def test_res(self):
+        res1 = 1 * 93.0351
+        res2 = 2 * 99.0111
+        res3 = 3 * 12.6911
+        self.assertEqual(res1, currenciesCalc(1, 'USD'))
+        self.assertEqual(res2, currenciesCalc(2, 'EUR'))
+        self.assertEqual(res3, currenciesCalc(3, 'CNY'))
 
     # def test_zero_mul(self):
     #     res1 = currenciesCalc(0, 'USD')
